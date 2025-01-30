a72fccde5e75dd81916ea941c5088ab39a8c37ee
pynicio <wapbenicio@gmail.com>
2025-01-30 18:38:12 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,15 @@
+from http.server import BaseHTTPRequestHandler
+from dash import Dash, html
 
+app = Dash(__name__)
+app.layout = html.Div("Hello, Vercel!")
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(app.index().encode())
+
+def handler(request):
+    return Handler()
